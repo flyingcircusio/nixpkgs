871c9f5f58f66d106d8c4e6a4c45a55986e4a0d7
Bryan A. S <bryanasdev000@gmail.com>
2022-05-07 19:07:29 -0300

    mongodb: fix build and sanitize package
    mongodb: fix build for 4.4
    
    mongodb: fix build for 4.2
    
    mongodb: fix build for 4.0
    
    mongodb: 5.0.7 -> 5.0.8
    
    mongodb: default to mongodb-5_0
    
    mongodb: eol versions older than 4.0
    
    mongodb: format and rename
    
    mongodb: use gcc10 for 5.x
    
    mongodb: use gcc10 only on linux
    
    mongodb: update docs with eol and default version
    
    mongodb: rename 5.0 to v5_0
    
    mongodb: fix typo in doc
    
    mongodb: change 5.x top level entry
    
    mongodb: use stateversion to determine mongodb pkg
    
    mongodb: use fetchpatch for gentoo patches
    
    mongodb: drop local patch from gentoo

--- pkgs/servers/nosql/mongodb/v4_4.nix
+++ pkgs/servers/nosql/mongodb/v4_4.nix
@@ -2,21 +2,18 @@
 
 let
   buildMongoDB = callPackage ./mongodb.nix {
-    inherit sasl;
-    inherit boost;
-    inherit Security;
-    inherit CoreFoundation;
-    inherit cctools;
+    inherit sasl boost Security CoreFoundation cctools;
   };
-in buildMongoDB {
-  version = "4.2.19";
-  sha256 = "sha256-fngTHd+fSdHqiqQYOYS7o6P5eHybeZy3iNKkGzFmjTw=";
+in
+buildMongoDB {
+  version = "4.4.13";
+  sha256 = "sha256-ebg3R6P+tjRvizDzsl7mZzhTfqIaRJPfHBu0IfRvtS8=";
   patches = [
-    ./forget-build-dependencies-4-2.patch
+    ./forget-build-dependencies-4-4.patch
    (fetchpatch {
       name = "mongodb-4.4.1-gcc11.patch";
       url = "https://raw.githubusercontent.com/gentoo/gentoo/7168257cad6ea7c4856b01c5703d0ed5b764367c/dev-db/mongodb/files/mongodb-4.4.1-gcc11.patch";
       sha256 = "sha256-RvfCP462RG+ZVjcb23DgCuxCdfPl2/UgH8N7FgCghGI=";
     })
-  ] ++ lib.optionals stdenv.isDarwin [ ./asio-no-experimental-string-view-4-2.patch ];
+  ] ++ lib.optionals stdenv.isDarwin [ ./asio-no-experimental-string-view-4-4.patch ];
 }
