d14e9e81e333f190929beba0b1d561a7dbf7362a
Tobias Stenzel <ts@flyingcircus.io>
2023-02-15 20:33:23 +0100

    gitlab: make Git package configurable
    There have been multiple Git CVE that affected Gitlab. It's helpful for quick
    Git updates to make the Git package used by Gitlab/Gitaly configurable
    because overriding the nixpkgs git package causes mass package rebuilds.

--- nixos/modules/services/misc/gitlab.nix
+++ nixos/modules/services/misc/gitlab.nix
@@ -43,7 +43,7 @@ let
     prometheus_listen_addr = "localhost:9236"
 
     [git]
-    bin_path = "${pkgs.git}/bin/git"
+    bin_path = "${cfg.packages.git}/bin/git"
 
     [gitlab-shell]
     dir = "${cfg.packages.gitlab-shell}"
@@ -256,6 +256,14 @@ in {
         description = lib.mdDoc ''
           Enable the gitlab service.
         '';
+      };
+
+      packages.git = mkOption {
+        type = types.package;
+        default = pkgs.git;
+        defaultText = literalExpression "pkgs.git";
+        description = lib.mdDoc "Reference to the git package";
+        example = literalExpression "pkgs.git";
       };
 
       packages.gitlab = mkOption {
@@ -1125,7 +1133,7 @@ in {
       }
     ];
 
-    environment.systemPackages = [ pkgs.git gitlab-rake gitlab-rails cfg.packages.gitlab-shell ];
+    environment.systemPackages = [ cfg.packages.git gitlab-rake gitlab-rails cfg.packages.gitlab-shell ];
 
     systemd.targets.gitlab = {
       description = "Common target for all GitLab services.";
