57ffadb5f1a70ea7e17fd401f0a23e9bc217c2a7
Vladimír Čunát <v@cunat.cz>
2026-01-17 08:46:17 +0100

    libpng: 1.6.46 -> 1.6.54
    And refresh the APNG patches, too.
    https://github.com/pnggroup/libpng/blob/02f2b4f4699f0ef9111a6534f093b53732df4452/ANNOUNCE
    Medium severity fixes: CVE-2026-22695 + CVE-2026-22801
    
    (cherry picked from commit 6eb2c0976417d5311b4ede14818916c2b5169c1d,
    skipping several intermediate ones)
    
    Co-authored-by: Oliver Schmidt <os@flyingcircus.io>

--- pkgs/development/libraries/libpng/default.nix
+++ pkgs/development/libraries/libpng/default.nix
@@ -2,7 +2,6 @@
   lib,
   stdenv,
   fetchurl,
-  fetchpatch,
   zlib,
   apngSupport ? true,
   testers,
@@ -11,37 +10,22 @@
 assert zlib != null;
 
 let
-  patchVersion = "1.6.46";
+  patchVersion = "1.6.54";
   patch_src = fetchurl {
     url = "mirror://sourceforge/libpng-apng/libpng-${patchVersion}-apng.patch.gz";
-    hash = "sha256-Kb7C39BG71HVLz5TIPkfr/yWvge0HZy51D2d9Veg0wM=";
+    hash = "sha256-Vjj8RQDq9IRXtNfJ5VP9gqsB13MEpwB32DNoQp1E8bQ=";
   };
   whenPatched = lib.optionalString apngSupport;
 
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "libpng" + whenPatched "-apng";
-  version = "1.6.46";
+  version = "1.6.54";
 
   src = fetchurl {
     url = "mirror://sourceforge/libpng/libpng-${finalAttrs.version}.tar.xz";
-    hash = "sha256-86qLcAOZirkqTpkGwY0ZhT6Zn507ypvRZo9U+oFwfLE=";
+    hash = "sha256-AcnYowPJQewsURwUMSo7HTbO20Hi9RaMzaqF1TuIeAU=";
   };
-
-  patches = [
-    # see https://github.com/pnggroup/libpng/security/advisories/GHSA-7wv6-48j4-hj3g
-    (fetchpatch {
-      name = "CVE-2025-65018.1.patch";
-      url = "https://github.com/pnggroup/libpng/commit/16b5e3823918840aae65c0a6da57c78a5a496a4d.diff";
-      hash = "sha256-pC7G/6IUCWkt9nE5RHnn8jT24KAo91y3f3+d2Us/gB0=";
-    })
-    (fetchpatch {
-      name = "CVE-2025-65018.2.patch";
-      url = "https://github.com/pnggroup/libpng/commit/218612ddd6b17944e21eda56caf8b4bf7779d1ea.diff";
-      hash = "sha256-jleVnBrsrTmRJqDo87slb7f8yETh3HN9geEwNBRZaGY=";
-    })
-  ];
-
   postPatch =
     whenPatched "gunzip < ${patch_src} | patch -Np1"
     + lib.optionalString stdenv.hostPlatform.isFreeBSD ''
@@ -66,17 +50,17 @@ stdenv.mkDerivation (finalAttrs: {
     tests.pkg-config = testers.testMetaPkgConfig finalAttrs.finalPackage;
   };
 
-  meta = with lib; {
+  meta = {
     description =
       "Official reference implementation for the PNG file format" + whenPatched " with animation patch";
     homepage = "http://www.libpng.org/pub/png/libpng.html";
     changelog = "https://github.com/pnggroup/libpng/blob/v${finalAttrs.version}/CHANGES";
-    license = licenses.libpng2;
+    license = lib.licenses.libpng2;
     pkgConfigModules = [
       "libpng"
       "libpng16"
     ];
-    platforms = platforms.all;
-    maintainers = with maintainers; [ vcunat ];
+    platforms = lib.platforms.all;
+    maintainers = with lib.maintainers; [ vcunat ];
   };
 })
